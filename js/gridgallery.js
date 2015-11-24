4c127bb873022e6839e114ecf47e998b0b01dddf
Alexandra Day <cybersiberian@gmail.com>
2015-11-24 16:17:59 +0200

    merging

--- js/gridgallery.js
+++ js/gridgallery.js
@@ -174,9 +174,9 @@ var Grid = (function() {
 		// used to know if the preview will expand in a different row
 		previewPos = -1,
 		// extra amount of pixels to scroll the window
-		scrollExtra = 0,
+		scrollExtra = 1,
 		// extra margin when expanded (between preview overlay and the next items)
-		marginExpanded = 10,
+		marginExpanded = 1,
 		$window = $( window ), winsize,
 		$body = $( 'html, body' ),
 		// transitionend events
@@ -193,7 +193,7 @@ var Grid = (function() {
 		// default settings
 		settings = {
 			minHeight : 500,
-			speed : 350,
+			speed : 150,
 			easing : 'ease',
 			showVisitButton : true
 		};
