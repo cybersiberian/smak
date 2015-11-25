9381cfa01167ef56713c05448e473b79e0de92d7
Alexandr <obram22@gmail.com>
2015-11-25 10:57:46 +0200

    sliders

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    items: 5,
+    nav: true,
+    navText: ["<<", ">>"],
+    responsiveClass:true
+})
+
+$('.owl-carousel').owlCarousel({
+	baseClass: "owl-quot",
+    loop:true,
+    items: 1,
+    nav: true,
+    navText: ["<<", ">>"],
+    responsiveClass:true
+})
